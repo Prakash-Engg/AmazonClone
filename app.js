2b9b579058fbe1604e1d933a9e6e4035945704e4
prakashsharma <prakashsharma7348@gmail.com>
2023-03-16 02:32:39 +0530

    changes

--- app.js
+++ app.js
@@ -23,6 +23,24 @@ app.use(
     credentials: true,
   })
 );
+
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://idyllic-strudel-90a591.netlify.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 app.use(router);
 
 const port = process.env.PORT || 8000;
