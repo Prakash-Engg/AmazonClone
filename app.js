e68a6468a1e4aa5dd7524ea6f8357bd168d58a31
prakashsharma <prakashsharma7348@gmail.com>
2023-03-16 03:34:16 +0530

    changes

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Headers",
     "X-Requested-With,content-type"
   );
-  // res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
 
