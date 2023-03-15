b55ac74de896f76e8e977effd3c5248eaeeafbd4
prakashsharma <prakashsharma7348@gmail.com>
2023-03-16 00:55:29 +0530

    changes

--- app.js
+++ app.js
@@ -20,7 +20,10 @@ app.use(cors());
 app.use(router);
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://amazonclonebackend.onrender.com"
+  );
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
