68667453077098329fbd6936642091c0f640d1e1
prakashsharma <prakashsharma7348@gmail.com>
2023-03-17 23:13:34 +0530

    changes

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser(""));
 app.use(
   cors({
-    origin: "https://serene-conkies-c4c250.netlify.app",
+    origin: "https://steady-wisp-5a7742.netlify.app",
     methods: ["PUT", "GET", "DELETE", "POST"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
@@ -29,7 +29,7 @@ app.use(
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://serene-conkies-c4c250.netlify.app"
+    "https://steady-wisp-5a7742.netlify.app"
   );
   next();
 });
