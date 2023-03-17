cccb3bebc77121b3e73f32f7cd016d8f1e280901
prakashsharma <prakashsharma7348@gmail.com>
2023-03-17 23:55:22 +0530

    changes

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,27 +1,17 @@
 const jwt = require("jsonwebtoken");
 const USER = require("../models/userSchema");
-// this is a middleware function to authenticate users for each & every API which we pass as a 2nd parameter
 const config = process.env;
 
 const authenticate = async (req, res, next) => {
-  // Modified to check for token in multiple sources
-  const token =
-    req.body.token ||
-    req.query.token ||
-    req.params.token ||
-    req.headers.authorization ||
-    req.headers["x-access-token"];
-  // Added check for Authorization header
-  if (req.headers.authorization) {
-    // Modified to extract token from Authorization header
-    const authHeader = req.headers.authorization;
-    const token = authHeader.split(" ")[1];
-  }
+  const authHeader = req.headers.authorization;
+  const token = authHeader.split(" ")[1];
+
   console.log(`line12 router.js ${token}`);
 
   if (!token) {
     return res.status(403).send("A token is required for authentication");
   }
+
   try {
     const decoded = jwt.verify(token, config.JWT_SECRET);
     console.log(decoded);
@@ -42,6 +32,7 @@ const authenticate = async (req, res, next) => {
   } catch (error) {
     return res.status(401).send("Invalid Token");
   }
+
   return next();
 };
 
