5188d6b532ccb2d7dc9cf96996801de9abc7f463
prakashsharma <prakashsharma7348@gmail.com>
2023-03-18 16:36:02 +0530

    first commit

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser(""));
 app.use(
   cors({
-    origin: "https://boisterous-parfait-5ea54f.netlify.app",
+    origin: "https://leafy-sfogliatella-97c90a.netlify.app",
     methods: ["PUT", "GET", "DELETE", "POST"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
@@ -29,7 +29,7 @@ app.use(
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://boisterous-parfait-5ea54f.netlify.app"
+    "https://leafy-sfogliatella-97c90a.netlify.app"
   );
   next();
 });
