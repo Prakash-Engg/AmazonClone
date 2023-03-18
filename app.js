1bd7b2e126b15ee75f237ead5c7ad0cd1689bee1
prakashsharma <prakashsharma7348@gmail.com>
2023-03-18 11:22:12 +0530

    first commit

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser(""));
 app.use(
   cors({
-    origin: "https://voluble-chimera-6557d7.netlify.app",
+    origin: "https://meek-yeot-50a2c0.netlify.app",
     methods: ["PUT", "GET", "DELETE", "POST"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
@@ -29,7 +29,7 @@ app.use(
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://voluble-chimera-6557d7.netlify.app"
+    "https://meek-yeot-50a2c0.netlify.app"
   );
   next();
 });
